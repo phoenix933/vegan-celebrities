de4839f6c78240439df2847974b728de99f0cc17
crashdev226 <midashand44@gmail.com>
2019-11-18 22:33:26 +0200

    celebrity list filter updated

--- src/app/modules/celebrities/models/celebrity-list-filter.ts
+++ src/app/modules/celebrities/models/celebrity-list-filter.ts
@@ -1,5 +1,10 @@
+import { Sex } from './../../../enums';
+
 export interface CelebrityListFilter {
     search?: string;
+    category?: number;
+    sex?: Sex;
+    country?: string;
     limit?: number;
     offset?: number;
 }
