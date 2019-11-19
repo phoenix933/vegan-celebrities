44d929a7f93250b9454c4a6e7e424e2f86f18876
crashdev226 <midashand44@gmail.com>
2019-11-19 21:43:46 +0200

    show edit button conditionally

--- src/app/modules/celebrities/containers/celebrity-detail/celebrity-detail.component.ts
+++ src/app/modules/celebrities/containers/celebrity-detail/celebrity-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
+import { UserRole } from './../../../auth/enums';
+import { AuthService } from './../../../auth/services';
 import { CelebritiesService } from './../../services';
 import { Celebrity } from '../../models';
 
@@ -13,15 +15,20 @@ import { Celebrity } from '../../models';
 })
 export class CelebrityDetailComponent implements OnInit {
     celebrity$: Observable<Celebrity>;
+    userRole$: Observable<UserRole>;
+
+    UserRole = UserRole;
 
     constructor(
         private _route: ActivatedRoute,
-        private _celebritiesService: CelebritiesService
+        private _celebritiesService: CelebritiesService,
+        private _authService: AuthService
     ) {
     }
 
     ngOnInit(): void {
         this.celebrity$ = this._celebritiesService.selectedCelebrity$;
+        this.userRole$ = this._authService.userRole$;
 
         this._route.paramMap
             .subscribe(paramMap => {
